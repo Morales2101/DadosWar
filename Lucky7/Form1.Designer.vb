cf4c450062e207037b97a6f157e3ed7263fe1610
M113750 <M113750@SA0032292>
2018-02-05 17:10:09 -0200

    Adicionar arquivos de projeto.

---
+++ Lucky7/Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SairToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnAtaque = New System.Windows.Forms.Button()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.btnDefesa = New System.Windows.Forms.Button()
+        Me.lbl5 = New System.Windows.Forms.Label()
+        Me.lbl6 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnVenc = New System.Windows.Forms.Button()
+        Me.lbldado1 = New System.Windows.Forms.Label()
+        Me.lbldado2 = New System.Windows.Forms.Label()
+        Me.lbldado3 = New System.Windows.Forms.Label()
+        Me.lbldado6 = New System.Windows.Forms.Label()
+        Me.lbldado5 = New System.Windows.Forms.Label()
+        Me.lbldado4 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Font = New System.Drawing.Font("Verdana", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl1.Location = New System.Drawing.Point(61, 74)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(43, 42)
+        Me.lbl1.TabIndex = 3
+        Me.lbl1.Text = "0"
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Font = New System.Drawing.Font("Verdana", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl2.Location = New System.Drawing.Point(260, 74)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(43, 42)
+        Me.lbl2.TabIndex = 4
+        Me.lbl2.Text = "0"
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Font = New System.Drawing.Font("Verdana", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl3.Location = New System.Drawing.Point(451, 74)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(43, 42)
+        Me.lbl3.TabIndex = 5
+        Me.lbl3.Text = "0"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SairToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(555, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SairToolStripMenuItem
+        '
+        Me.SairToolStripMenuItem.Name = "SairToolStripMenuItem"
+        Me.SairToolStripMenuItem.Size = New System.Drawing.Size(38, 20)
+        Me.SairToolStripMenuItem.Text = "Sair"
+        '
+        'btnAtaque
+        '
+        Me.btnAtaque.Font = New System.Drawing.Font("Verdana", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAtaque.Location = New System.Drawing.Point(211, 119)
+        Me.btnAtaque.Name = "btnAtaque"
+        Me.btnAtaque.Size = New System.Drawing.Size(136, 52)
+        Me.btnAtaque.TabIndex = 7
+        Me.btnAtaque.Text = "Rolar"
+        Me.btnAtaque.UseVisualStyleBackColor = True
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Font = New System.Drawing.Font("Verdana", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl4.Location = New System.Drawing.Point(61, 238)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(43, 42)
+        Me.lbl4.TabIndex = 9
+        Me.lbl4.Text = "0"
+        '
+        'btnDefesa
+        '
+        Me.btnDefesa.Font = New System.Drawing.Font("Verdana", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDefesa.Location = New System.Drawing.Point(211, 283)
+        Me.btnDefesa.Name = "btnDefesa"
+        Me.btnDefesa.Size = New System.Drawing.Size(136, 52)
+        Me.btnDefesa.TabIndex = 12
+        Me.btnDefesa.Text = "Rolar"
+        Me.btnDefesa.UseVisualStyleBackColor = True
+        '
+        'lbl5
+        '
+        Me.lbl5.AutoSize = True
+        Me.lbl5.Font = New System.Drawing.Font("Verdana", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl5.Location = New System.Drawing.Point(260, 238)
+        Me.lbl5.Name = "lbl5"
+        Me.lbl5.Size = New System.Drawing.Size(43, 42)
+        Me.lbl5.TabIndex = 11
+        Me.lbl5.Text = "0"
+        '
+        'lbl6
+        '
+        Me.lbl6.AutoSize = True
+        Me.lbl6.Font = New System.Drawing.Font("Verdana", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl6.Location = New System.Drawing.Point(451, 238)
+        Me.lbl6.Name = "lbl6"
+        Me.lbl6.Size = New System.Drawing.Size(43, 42)
+        Me.lbl6.TabIndex = 13
+        Me.lbl6.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(219, 187)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 32)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Defesa"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(219, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 32)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Ataque"
+        '
+        'btnVenc
+        '
+        Me.btnVenc.Location = New System.Drawing.Point(29, 161)
+        Me.btnVenc.Name = "btnVenc"
+        Me.btnVenc.Size = New System.Drawing.Size(90, 23)
+        Me.btnVenc.TabIndex = 17
+        Me.btnVenc.Text = "Quem ganhou?"
+        Me.btnVenc.UseVisualStyleBackColor = True
+        '
+        'lbldado1
+        '
+        Me.lbldado1.AutoSize = True
+        Me.lbldado1.Location = New System.Drawing.Point(62, 61)
+        Me.lbldado1.Name = "lbldado1"
+        Me.lbldado1.Size = New System.Drawing.Size(42, 13)
+        Me.lbldado1.TabIndex = 18
+        Me.lbldado1.Text = "Dado 1"
+        '
+        'lbldado2
+        '
+        Me.lbldado2.AutoSize = True
+        Me.lbldado2.Location = New System.Drawing.Point(261, 61)
+        Me.lbldado2.Name = "lbldado2"
+        Me.lbldado2.Size = New System.Drawing.Size(42, 13)
+        Me.lbldado2.TabIndex = 19
+        Me.lbldado2.Text = "Dado 2"
+        '
+        'lbldado3
+        '
+        Me.lbldado3.AutoSize = True
+        Me.lbldado3.Location = New System.Drawing.Point(452, 61)
+        Me.lbldado3.Name = "lbldado3"
+        Me.lbldado3.Size = New System.Drawing.Size(42, 13)
+        Me.lbldado3.TabIndex = 20
+        Me.lbldado3.Text = "Dado 3"
+        '
+        'lbldado6
+        '
+        Me.lbldado6.AutoSize = True
+        Me.lbldado6.Location = New System.Drawing.Point(452, 225)
+        Me.lbldado6.Name = "lbldado6"
+        Me.lbldado6.Size = New System.Drawing.Size(42, 13)
+        Me.lbldado6.TabIndex = 23
+        Me.lbldado6.Text = "Dado 3"
+        '
+        'lbldado5
+        '
+        Me.lbldado5.AutoSize = True
+        Me.lbldado5.Location = New System.Drawing.Point(261, 225)
+        Me.lbldado5.Name = "lbldado5"
+        Me.lbldado5.Size = New System.Drawing.Size(42, 13)
+        Me.lbldado5.TabIndex = 22
+        Me.lbldado5.Text = "Dado 2"
+        '
+        'lbldado4
+        '
+        Me.lbldado4.AutoSize = True
+        Me.lbldado4.Location = New System.Drawing.Point(62, 225)
+        Me.lbldado4.Name = "lbldado4"
+        Me.lbldado4.Size = New System.Drawing.Size(42, 13)
+        Me.lbldado4.TabIndex = 21
+        Me.lbldado4.Text = "Dado 1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(555, 347)
+        Me.Controls.Add(Me.lbldado6)
+        Me.Controls.Add(Me.lbldado5)
+        Me.Controls.Add(Me.lbldado4)
+        Me.Controls.Add(Me.lbldado3)
+        Me.Controls.Add(Me.lbldado2)
+        Me.Controls.Add(Me.lbldado1)
+        Me.Controls.Add(Me.btnVenc)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl6)
+        Me.Controls.Add(Me.btnDefesa)
+        Me.Controls.Add(Me.lbl5)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.btnAtaque)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "War"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents SairToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents btnAtaque As Button
+    Friend WithEvents lbl4 As Label
+    Friend WithEvents btnDefesa As Button
+    Friend WithEvents lbl5 As Label
+    Friend WithEvents lbl6 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnVenc As Button
+    Friend WithEvents lbldado1 As Label
+    Friend WithEvents lbldado2 As Label
+    Friend WithEvents lbldado3 As Label
+    Friend WithEvents lbldado6 As Label
+    Friend WithEvents lbldado5 As Label
+    Friend WithEvents lbldado4 As Label
+End Class
---
+++ Lucky7/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub btnAtaque_Click(sender As Object, e As EventArgs) Handles btnAtaque.Click
+        lbl1.Text = CStr(Int(Rnd() * 6))
+        lbl2.Text = CStr(Int(Rnd() * 6))
+        lbl3.Text = CStr(Int(Rnd() * 6))
+
+
+        If (lbl1.Text = "1") Or (lbl1.Text = "2") Or (lbl1.Text = "3") Or (lbl1.Text = "4") Or
+               (lbl1.Text = "5") Or (lbl1.Text = "6") Then
+            If (lbl2.Text = "1") Or (lbl2.Text = "2") Or (lbl2.Text = "3") Or (lbl2.Text = "4") Or
+             (lbl2.Text = "5") Or (lbl2.Text = "6") Then
+                If (lbl3.Text = "1") Or (lbl3.Text = "2") Or (lbl3.Text = "3") Or (lbl3.Text = "4") Or
+           (lbl3.Text = "5") Or (lbl3.Text = "6") Then
+                    Beep()
+                End If
+                Beep()
+            End If
+            Beep()
+        End If
+
+    End Sub
+
+
+    Private Sub btnDefesa_Click(sender As Object, e As EventArgs) Handles btnDefesa.Click
+        lbl4.Text = CStr(Int(Rnd() * 6))
+        lbl5.Text = CStr(Int(Rnd() * 6))
+        lbl6.Text = CStr(Int(Rnd() * 6))
+
+        If (lbl1.Text = "1") Or (lbl1.Text = "2") Or (lbl1.Text = "3") Or (lbl1.Text = "4") Or
+               (lbl1.Text = "5") Or (lbl1.Text = "6") Then
+            If (lbl2.Text = "1") Or (lbl2.Text = "2") Or (lbl2.Text = "3") Or (lbl2.Text = "4") Or
+             (lbl2.Text = "5") Or (lbl2.Text = "6") Then
+                If (lbl3.Text = "1") Or (lbl3.Text = "2") Or (lbl3.Text = "3") Or (lbl3.Text = "4") Or
+           (lbl3.Text = "5") Or (lbl3.Text = "6") Then
+                    Beep()
+                End If
+                Beep()
+            End If
+            Beep()
+        End If
+
+    End Sub
+
+    Private Sub SairToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SairToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnVenc.Click
+        'Três dados maiores que a defesa
+        If (lbl1.Text > lbl4.Text) And (lbl2.Text > lbl5.Text) And (lbl3.Text > lbl6.Text) Then
+            MessageBox.Show("Ataque venceu!")
+        ElseIf (lbl1.Text > lbl4.Text) And (lbl2.Text > lbl5.Text) And (lbl3.Text < lbl6.Text) Then
+            MessageBox.Show("Ataque venceu!")
+        ElseIf (lbl1.Text < lbl4.Text) And (lbl2.Text > lbl5.Text) And (lbl3.Text > lbl6.Text) Then
+            MessageBox.Show("Ataque venceu!")
+        ElseIf (lbl1.Text > lbl4.Text) And (lbl2.Text < lbl5.Text) And (lbl3.Text > lbl6.Text) Then
+            MessageBox.Show("Ataque venceu!")
+        Else
+            MessageBox.Show("Defesa venceu!")
+        End If
+    End Sub
+End Class
